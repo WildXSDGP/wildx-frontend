efdf1dcf1f90271a0af5a53f36cbfc0203f2b266
Banuka Malshan <bmagamulla@gmail.com>
2026-02-17 16:16:00 +0530

    1. Add internet permission to AndroidManifest.xml. 2. Create data folder and add animales list for Dropdown menue

---
+++ wildx_frontend/lib/data/national_park_data.dart
@@ -0,0 +1,15 @@
+import 'package:wildx_frontend/models/national_park_model.dart';
+import 'package:wildx_frontend/services/national_park_service.dart' as service;
+
+class NationalParkData {
+  static const List<String> animalTypes = [
+    'Asian Elephant',
+    'Sri Lankan Leoperd',
+    'Spotted Dear',
+    'Crocodiles',
+    'Water Buffalo',
+    'Sloth Bear',
+  ]; // create list to store animal types for dropdown menue
+
+  // static const List<NationalPark> allParksList = [service.NationalParkService().];
+}
