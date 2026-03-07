cea78e17e8c01041b0b4dbd20daa4689803da72b
Dewthilini-D <dewthilini6@gmail.com>
2026-03-07 14:44:38 +0530

    create ParkDetailsScreen class to fetch park details display from database

---
+++ wildx_frontend/lib/screens/park_details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import '../models/national_park_model.dart';
+import '../widjets/info_card.dart';
+import '../widjets/rule_item.dart';
+
+class ParkDetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
