cfaf6631fa6fb24f12ba3b2e3890e9e43381cae7
Banuka Malshan <bmagamulla@gmail.com>
2026-03-07 12:24:28 +0530

    Implement the rule item widget ui features

--- wildx_frontend/lib/widjets/rule_item.dart
+++ wildx_frontend/lib/widjets/rule_item.dart
@@ -5,6 +5,16 @@ class RuleItem extends StatelessWidget {
   const RuleItem({Key? key, required this.rule}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Icon(Icons.check_circle, size: 20, color: Colors.green),
+          const SizedBox(width: 8),
+          Expanded(child: Text(rule)),
+        ],
+      ),
+    );
   }
 }
