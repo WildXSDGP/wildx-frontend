3b0443cba8310d13c42586c1dd538c8c566ddfb1
Dewthilini-D <dewthilini6@gmail.com>
2026-03-07 16:54:21 +0530

    Implement ParkSearchScreen state class

---
+++ wildx_frontend/lib/screens/park_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../data/national_park_data.dart';
+import '../models/national_park_model.dart';
+import '../services/national_park_service.dart';
+import '../widjets/animal_search_bar.dart';
+import '../widjets/empty_state.dart';
+import '../widjets/park_card.dart';
+
+class ParkSearchScreen extends StatefulWidget {
+  const ParkSearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
