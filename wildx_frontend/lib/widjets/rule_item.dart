38ee92c66105722c7e2b9e085377ee625f7e4fb5
Banuka Malshan <bmagamulla@gmail.com>
2026-03-07 12:22:15 +0530

    Create String instance named rule ,to assign runes retrieve from backend. and Intialized it

--- wildx_frontend/lib/widjets/rule_item.dart
+++ wildx_frontend/lib/widjets/rule_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class RuleItem extends StatelessWidget {
+  final String rule;
+  const RuleItem({Key? key, required this.rule}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Center();
