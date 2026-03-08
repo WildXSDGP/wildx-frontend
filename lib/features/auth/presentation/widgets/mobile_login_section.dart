7ebda353f786f25754b47f7398282ef0428efa82
thanuvim <thanuvimuthukumarana@gmail.com>
2026-03-09 01:30:10 +0530

    feat: mobile login section base scaffold

---
+++ lib/features/auth/presentation/widgets/mobile_login_section.dart
@@ -0,0 +1,22 @@
+
+// Commit 1: Mobile Login Section - Base Scaffold
+
+import 'package:flutter/material.dart';
+
+class MobileLoginSection extends StatefulWidget {
+  const MobileLoginSection({super.key});
+
+  @override
+  State<MobileLoginSection> createState() => _MobileLoginSectionState();
+}
+
+class _MobileLoginSectionState extends State<MobileLoginSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        SizedBox(height: 10),
+      ],
+    );
+  }
+}
