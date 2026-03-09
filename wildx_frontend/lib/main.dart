9ed73a73fb4021ebe2ba089b26c9349447a610c0
MewanyaN05 <narthanilokuge@gmail.com>
2026-03-09 19:32:37 +0530

    Refactor main application structure and add routing for accommodations

--- wildx_frontend/lib/main.dart
+++ wildx_frontend/lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+
+import 'routes/app_routes.dart';
 import 'screens/accommodation_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: AccommodationScreen(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.accommodations,
+      routes: {
+        AppRoutes.accommodations: (_) => const AccommodationScreen(),
+        // AppRoutes.accommodationDetail: (_) => const AccommodationDetailScreen(),
+      },
+    );
+  }
 }
---
+++ wildx_frontend/lib/routes/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  AppRoutes._();
+
+  static const String accommodations = '/accommodations';
+  static const String accommodationDetail = '/accommodation-detail';
+}
