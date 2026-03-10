c6fb4ffc0ce2bf5392ba989d213dba4bfa35a398
Banuka Malshan <bmagamulla@gmail.com>
2026-03-10 16:55:02 +0530

    Set API key to the IOS

--- wildx_frontend/ios/Runner/AppDelegate.swift
+++ wildx_frontend/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyB5XvhNg9qN59PnsPyU3M5s5Xt3xy8Xc0o")
+    GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
