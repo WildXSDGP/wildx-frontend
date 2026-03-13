7e54db30372ca7ecbc2d7e42b72a235037f5238e
Savith29 <savithransika29@gmail.com>
2026-03-13 13:28:40 +0530

    feat(auth): add base UserModel fields and constructor

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,1 +1,17 @@
 enum AuthType { phone, email, biometric }
+
+// This is the model for WildX user data
+class UserModel {
+  final String id;
+  final String? phoneNumber;
+  final String? email;
+  final AuthType authType;
+
+
+  const UserModel({
+    required this.id,
+    this.phoneNumber,
+    this.email,
+    required this.authType,
+  });
+}
