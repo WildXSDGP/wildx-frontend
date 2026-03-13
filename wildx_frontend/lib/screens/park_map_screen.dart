a3be32e37de3352c548361458cfd53342c06ab32
Dewthilini-D <dewthilini6@gmail.com>
2026-03-13 20:27:05 +0530

    Create class to initialized map.

--- wildx_frontend/lib/screens/park_map_screen.dart
+++ wildx_frontend/lib/screens/park_map_screen.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../models/national_park_model.dart';
+
+class ParkMapScreen extends StatefulWidget {
+  final NationalPark park;
+
+  const ParkMapScreen({Key? key, required this.park}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
