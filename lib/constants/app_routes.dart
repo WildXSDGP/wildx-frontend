135baa2c0facd5a0b911d5d4c7a7de95a1e9768c
Savith29 <savithransika29@gmail.com>
2026-03-14 23:46:41 +0530

    define application route constants for navigation

---
+++ lib/constants/app_routes.dart
@@ -0,0 +1,9 @@
+abstract class AppRoutes {
+  static const String home    = '/';
+  static const String map     = '/map';
+  static const String parks   = '/parks';
+  static const String report  = '/report';
+  static const String gallery = '/gallery';
+  static const String sos     = '/sos';
+  static const String profile = '/profile';
+}
