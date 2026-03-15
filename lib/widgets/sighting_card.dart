5ed2ec5df3f5c502ab0df72e0ce1169f3ca4cd6f
Savith29 <savithransika29@gmail.com>
2026-03-15 12:50:40 +0530

    add _CategoryBadge and finalize SightingCard widget assembly

--- lib/widgets/sighting_card.dart
+++ lib/widgets/sighting_card.dart
@@ -97,3 +97,16 @@ class _SightingInfo extends StatelessWidget {
     );
   }
 }
+class _CategoryBadge extends StatelessWidget {
+  final String category;
+  const _CategoryBadge({required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      decoration: AppDecorations.primaryBadge,
+      child: Text(category, style: AppTextStyles.badge),
+    );
+  }
+}
