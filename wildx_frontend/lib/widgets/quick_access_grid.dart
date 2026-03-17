6f0c3c5af8aa7dbd8f188862b73dbf2707d121db
Savith29 <savithransika29@gmail.com>
2026-03-17 13:53:25 +0530

    add QuickAccessCard with animation

--- wildx_frontend/lib/widgets/quick_access_grid.dart
+++ wildx_frontend/lib/widgets/quick_access_grid.dart
@@ -32,3 +32,69 @@ class QuickAccessGrid extends StatelessWidget {
     );
   }
 }
+/// Exported so it can be used standalone if needed.
+class QuickAccessCard extends StatefulWidget {
+  final QuickAccessItem item;
+  final VoidCallback onTap;
+
+  const QuickAccessCard({
+    super.key,
+    required this.item,
+    required this.onTap,
+  });
+
+  @override
+  State<QuickAccessCard> createState() => _QuickAccessCardState();
+}
+
+class _QuickAccessCardState extends State<QuickAccessCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnim;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+    );
+    _scaleAnim = Tween<double>(begin: 1.0, end: 0.92).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => _controller.forward(),
+      onTapUp: (_) {
+        _controller.reverse();
+        widget.onTap();
+      },
+      onTapCancel: () => _controller.reverse(),
+      child: AnimatedBuilder(
+        animation: _scaleAnim,
+        builder: (_, child) =>
+            Transform.scale(scale: _scaleAnim.value, child: child),
+        child: Container(
+          decoration: AppDecorations.card,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _IconBox(item: widget.item),
+              const SizedBox(height: 8),
+              Text(widget.item.label, style: AppTextStyles.label),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
