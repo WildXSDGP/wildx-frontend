6d96f28e35cdf544ec3e5310a698858f0afb2c5f
thanuvim <thanuvimuthukumarana@gmail.com>
2026-03-17 22:10:29 +0530

    implement date formatting helper for membership display

--- wildx_frontend/lib/models/user_model.dart
+++ wildx_frontend/lib/models/user_model.dart
@@ -74,6 +74,14 @@ class UserModel {
     }
   }
 
+  String get memberSinceLabel {
+    const months = [
+      'Jan','Feb','Mar','Apr','May','Jun',
+      'Jul','Aug','Sep','Oct','Nov','Dec',
+    ];
+    return '${months[memberSince.month - 1]} ${memberSince.year}';
+  }
+
   
 }
 
